3fda518b4d01e9e1b9cab5fbf1008414a3918e74
stanjhaei <ceejay.orji@theitsolutions.io>
2019-11-16 13:17:11 +0100

    Changed port

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-exports.PORT = process.env.PORT || 9000;
+exports.PORT = process.env.PORT || 9002;
 exports.MONGO_ERR = 'MongoError';
 exports.DUP_ERR = 11000;
 exports.DEFAULT_ERR_MSG = 'OOPS! Sumfin goofed!!';
